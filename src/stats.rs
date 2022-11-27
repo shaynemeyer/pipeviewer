ea4a1ac94215c32a739df2e53d1c48a2171bb34e
Shayne Meyer <shayne.meyer@gmail.com>
2022-11-27 06:24:57 -0800

    refactored to use channels

--- src/stats.rs
+++ src/stats.rs
@@ -1,10 +1,25 @@
-pub fn stats(silent: bool, num_read: usize, total_bytes: &mut usize, last: bool) {
-    *total_bytes += num_read;
+use crossbeam::channel::Receiver;
+use std::io::Result;
 
-    if !silent {
-        eprint!("\r{}", total_bytes);
-        if last {
-            eprintln!();
+pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
+    let mut total_bytes = 0;
+
+    loop {
+        let num_bytes = stats_rx.recv().unwrap();
+        total_bytes += num_bytes;
+
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+
+        if num_bytes == 0 {
+            break;
         }
     }
+
+    if !silent {
+        eprintln!();
+    }
+
+    Ok(())
 }
